7edcd521fadd6336ed8e4c73bc7a9d49a690bd9c
Rouzrise <rouzrise@gmail.com>
2018-07-21 13:25:29 +0300

    Add BookUrl, title & authors to be rendered in search results

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 import ListBooks from './Components/ListBooks';
 import {Route, Link} from 'react-router-dom';
 import * as BooksAPI from './BooksAPI';
+import EscapeRegEx from 'escape-string-regexp';
 import sortBy from 'sort-by';
 
 class BooksApp extends React.Component {
@@ -34,21 +35,40 @@ class BooksApp extends React.Component {
                 }
               })
             }
-            this.setState({showingBooks})
+
+            this.setState({showingBooks: showingBooks})
           })
         }
         else {
-          this.setState({showingBooks})
+          this.setState({showingBooks: showingBooks})
         }
        }
+
+  // updateQuery = (query) => {
+  //   this.setState ({query: query})
+  
+  //   if (query) {
+  //     BooksAPI.search(query).then(response => {
+
+  //       if (response.length) {
+      
+  //       this.setState({ 
+  //         showingBooks: response
+  //       })
+
+  //       }
+  //     })
+  //   }
+  // }
+  
 
   render() {
 
     //DESTRUCTURING
-    const { query, showingBooks} = this.state;
+    const { query, showingBooks, books} = this.state;
 
-    //SORTING RESULTS ALPHABETICALLY
-    // showingBooks.sort(sortBy('name'));
+
+
 
     return (
       <div className="app">
@@ -67,7 +87,31 @@ class BooksApp extends React.Component {
             </div>
             <div className="search-books-results">
               <ol className="books-grid">
-              {console.log('Hello', showingBooks)}</ol>
+              {showingBooks.map((book) => 
+                (
+                  <li>
+                  <div className="book">
+                    <div className="book-top">
+                      <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: book.imageLinks ? `url(${book.imageLinks.thumbnail})` : '' }}></div>
+                      <div className="book-shelf-changer">
+                        <select>
+                          <option value="move" disabled>Move to...</option>
+                          <option value="currentlyReading">Currently Reading</option>
+                          <option value="wantToRead">Want to Read</option>
+                          <option value="read">Read</option>
+                          <option value="none">None</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="book-title">{book.title ? book.title : ''}</div>
+                    <div className="book-authors">{book.authors ? book.authors : ''}</div>
+                  </div>
+                </li> 
+                )
+              )
+              }
+            
+              {console.log('Hello', books)}</ol>
             </div>
           </div>
         )} />
