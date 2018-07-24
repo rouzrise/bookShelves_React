4a4f21fef2a9b10075ac0bfa8f18c86066dfa0aa
Rouzrise <rouzrise@gmail.com>
2018-07-24 15:34:04 +0300

    Set state for query and showingBooks in Search
    It doesn't keep query and showingBooks (search results) every time we
    go back-forth between pages

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ import * as BooksAPI from './BooksAPI';
 
 class BooksApp extends React.Component {
   state = {
-    books: [],
-    query: '',
-    showingBooks: []
+    books: []
   }
 
   componentDidMount() {
@@ -34,51 +32,17 @@ class BooksApp extends React.Component {
     }
   } 
 
-
-  setShelf = (books) => {
-    books.map(book => (this.state.books.filter((b) => b.id === book.id).map(b => book.shelf = b.shelf)))
-  }
-
-   updateQuery = (query) => {
-    this.setState({query: query}) //this is done so that everything you print in searchbox could be reflected on the page
-    
-      if (query.trim() === '') {
-          this.setState({showingBooks: []})
-          console.log("print some possible words in query - 'Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS'")
-      }
-
-      else {
-        BooksAPI.search(query).then(response => {
-        if (response.error) {
-          this.setState({showingBooks: []})
-                          // automatically making query empty is not ok here as soon as it can confuse user
-                          // query: ''}) 
-          console.log("Error:", response.error, ". If error you see is 'empty query' it means you are searching for invalid words. Please try these ones - 'Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS'")
-
-        }
-        else {
-          response.map(b => b.shelf='none')
-          this.setShelf(response)
-          this.setState({showingBooks: response})
-        }
-      })
-    }
-   }
-
-
   render() {
 
     //DESTRUCTURING
-    const { query, showingBooks, books} = this.state;
+    const { books} = this.state;
 
     return (
       <div className="app">
 
       <Route exact path='/search' render={() => (
-      <Search query={query} 
-              updateQuery={this.updateQuery} 
-              updateBookShelf={this.updateBookShelf} 
-              showingBooks={showingBooks}
+      <Search updateBookShelf={this.updateBookShelf} 
+              books={books}
               />
       )} />
 
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,14 +1,51 @@
 import React from 'react'
 import Book from './Book'
 import {Link} from 'react-router-dom';
+import * as BooksAPI from './../BooksAPI';
 import PropTypes from 'prop-types';
 
 class Search extends React.Component {
 
+  state = {
+    query: '',
+    showingBooks: []
+  }
+
+  setShelf = (books) => {
+    books.map(book => (this.props.books.filter((b) => b.id === book.id).map(b => book.shelf = b.shelf)))
+  }
+
+   updateQuery = (query) => {
+    this.setState({query: query}) //this is done so that everything you print in searchbox could be reflected on the page
+    
+      if (query.trim() === '') {
+          this.setState({showingBooks: []})
+          console.log("print some possible words in query - 'Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS'")
+      }
+
+      else {
+        BooksAPI.search(query).then(response => {
+        if (response.error) {
+          this.setState({showingBooks: []})
+                          // automatically making query empty is not ok here as soon as it can confuse user
+                          // query: ''}) 
+          console.log("Error:", response.error, ". If error you see is 'empty query' it means you are searching for invalid words. Please try these ones - 'Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS'")
+
+        }
+        else {
+          response.map(b => b.shelf='none')
+          this.setShelf(response)
+          this.setState({showingBooks: response})
+        }
+      })
+    }
+   }
+
   render() {
 
     //DESTRUCTURING
-    const { query, showingBooks, updateQuery, updateBookShelf } = this.props;
+    const {updateBookShelf } = this.props;
+    const {query, showingBooks} = this.state;
 
     return (
           <div className="search-books">
@@ -22,7 +59,7 @@ class Search extends React.Component {
                 <input type="text" 
                         placeholder="Search by title or author"
                         value={query}
-                        onChange={(e) => updateQuery(e.target.value)}/>
+                        onChange={(e) => this.updateQuery(e.target.value)}/>
 
               </div>
             </div>
